774baee57e33306a9094244916cf71b2c5c3a58c
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-01-13 15:41:33 -0500

    refactor: add constants file for saving assets directories paths

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+// Project assets directories
+export const projectsAssetsDir = "/src/assets/projects/";
+export const projectsIconsDir = projectsAssetsDir + "icons/";
+export const projectsThumbnailsDir = projectsAssetsDir + "thumbnails/";
