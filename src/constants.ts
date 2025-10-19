fbc69768ab6ac2d03261f6a7daf0a556e47aee4c
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-10-19 17:43:17 -0500

    perf: import icons and add them to the map in constants.ts for better performance

--- src/constants.ts
+++ src/constants.ts
@@ -5,21 +5,26 @@ export const projectsAssetsDir = "/src/assets/projects/";
 export const projectsIconsDir = projectsAssetsDir + "icons/";
 export const projectsThumbnailsDir = projectsAssetsDir + "thumbnails/";
 
+const techIconImports = import.meta.glob<{ default: ImageMetadata }>(
+  "/src/assets/technologies/*.svg",
+  { eager: true }
+);
+
 // Technology icons src and alt map
 const techIconsCollection = await getCollection("technologyIcons")
-export const techIconsMap = new Map<string, { src: string, alt: string }>();
+export const techIconsMap = new Map<string, { metadata: ImageMetadata, alt: string }>();
 techIconsCollection.map(({ id, data: { src, alt } }) => {
+  const iconImport = techIconImports[src];
+  if (!iconImport) {
+    throw new Error(`Technology icon import not found for src: ${src}`);
+  }
+
   techIconsMap.set(id, {
-    src: src,
+    metadata: iconImport.default,
     alt: alt
   });
 });
 
-// Technology icon imports
-export const techIconImports = import.meta.glob<{ default: ImageMetadata }>(
-  "/src/assets/*/*.svg",
-  { eager: true }
-);
 
 // Project icons imports
 export const projectIconsImports = import.meta.glob<{ default: ImageMetadata }>("/src/assets/projects/icons/*.svg", { eager: true });
